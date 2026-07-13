2df526c46f1afff9344ef74f22b0864ad2ef64c1
EmyDev <115660028+em1dev@users.noreply.github.com>
2026-07-13 07:51:04 +0100

    add healthcheck and restart policy

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -11,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/health', (_, res) => {
+  res.status(200).send();
+});
+
 app.use('/', (req, _, next) => {
   console.log(`[${req.method}] - ${req.path}`);
   next();
